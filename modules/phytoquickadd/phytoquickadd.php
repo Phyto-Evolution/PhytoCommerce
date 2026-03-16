6822a1769b6f60479ebe0b5a75bc6054fff36cd1
Shivaramakrishnan <shiva.adam@gmail.com>
2026-03-16 08:26:20 +0530

    Add files via upload

---
+++ modules/phytoquickadd/phytoquickadd.php
@@ -0,0 +1,46 @@
+<?php
+if (!defined('_PS_VERSION_')) exit;
+
+class PhytoQuickAdd extends Module {
+
+    public function __construct() {
+        $this->name = 'phytoquickadd';
+        $this->tab = 'administration';
+        $this->version = '2.0.0';
+        $this->author = 'Phyto Evolution';
+        $this->need_instance = 0;
+        parent::__construct();
+        $this->displayName = 'Phyto Quick Add';
+        $this->description = 'Quickly add products and categories with optional AI descriptions';
+    }
+
+    public function install() {
+        return parent::install() && $this->installTab();
+    }
+
+    public function uninstall() {
+        return parent::uninstall() && $this->uninstallTab();
+    }
+
+    private function installTab() {
+        $tab = new Tab();
+        $tab->active = 1;
+        $tab->class_name = 'AdminPhytoQuickAdd';
+        $tab->name = [];
+        foreach (Language::getLanguages(true) as $lang) {
+            $tab->name[$lang['id_lang']] = 'Phyto Quick Add';
+        }
+        $tab->id_parent = (int)Tab::getIdFromClassName('AdminCatalog');
+        $tab->module = $this->name;
+        return $tab->add();
+    }
+
+    private function uninstallTab() {
+        $id_tab = (int)Tab::getIdFromClassName('AdminPhytoQuickAdd');
+        if ($id_tab) {
+            $tab = new Tab($id_tab);
+            return $tab->delete();
+        }
+        return true;
+    }
+}
